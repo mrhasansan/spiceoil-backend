18c63ae3c8d3797623b7f3e8773eea44e25775dd
Hasansan <marufhasansan@gmail.com>
2024-07-17 22:13:41 +0700

    re migrate prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 import { prisma } from "./libs/prisma";
 
 const app = new Hono();
